d9a27a3880354f319f76114addf7ebe629aa6659
IvanCaceresS <102769594+IvanCaceresS@users.noreply.github.com>
2023-11-07 18:08:33 -0300

    Lab4.js

--- Lab4.js
+++ Lab4.js
@@ -11,6 +11,54 @@
 
 (function() {
     'use strict';
+
+    //BORRANDO TODA LA PAGINA PARA HACER OTRA
+    const body = document.body;
+    while (body.firstChild) {
+        body.removeChild(body.firstChild);
+    }
+    setTimeout(function() {
+        const pElement = document.createElement('p');
+        const nuevoTexto = "En una tranquila aldea al pie de las montañas, vivía un anciano llamado Samuel. Samuel era conocido en la comunidad por sus historias mágicas y consejos sabios. Todos los atardeceres, se reunían niños y adultos en la plaza del pueblo para escuchar sus cuentos sobre mundos lejanos y criaturas místicas. Sus historias inspiraban a los habitantes y llenaban sus corazones de asombro y esperanza. La magia de Samuel no radicaba solo en sus relatos, sino en la forma en que conectaba a la comunidad, uniendo generaciones y recordándoles la belleza de la imaginación.En una tranquila aldea al pie de las montañas, vivía un anciano llamado Samuel. Samuel era conocido en la comunidad por sus historias mágicas y consejos sabios. Todos los atardeceres, se reunían niños y adultos en la plaza del pueblo para escuchar sus cuentos sobre mundos lejanos y criaturas místicas. Sus historias inspiraban a los habitantes y llenaban sus corazones de asombro y esperanza. La magia de Samuel no radicaba solo en sus relatos, sino en la forma en que conectaba a la comunidad, uniendo generaciones y recordándoles la belleza de la imaginación.En una tranquila aldea al pie de las montañas, vivía un anciano llamado Samuel. Samuel era conocido en la comunidad por sus historias mágicas y consejos sabios. Todos los atardeceres, se reunían niños y adultos en la plaza del pueblo para escuchar sus cuentos sobre mundos lejanos y criaturas místicas. Sus historias inspiraban a los habitantes y llenaban sus corazones de asombro y esperanza. La magia de Samuel no radicaba solo en sus relatos, sino en la forma en que conectaba a la comunidad, uniendo generaciones y recordándoles la belleza de la imaginación.En una tranquila aldea al pie de las montañas, vivía un anciano llamado Samuel. Samuel era conocido en la comunidad por sus historias mágicas y consejos sabios. Todos los atardeceres, se reunían niños y adultos en la plaza del pueblo para escuchar sus cuentos sobre mundos lejanos y criaturas místicas. Sus historias inspiraban a los habitantes y llenaban sus corazones de asombro y esperanza. La magia de Samuel no radicaba solo en sus relatos, sino en la forma en que conectaba a la comunidad, uniendo generaciones y recordándoles la belleza de la imaginación.";
+        pElement.textContent = nuevoTexto;
+        body.appendChild(pElement);
+
+        // Crea un elemento padre
+        const divPadre = document.createElement('div');
+        divPadre.id = 'miDivPadre'; // Puedes asignar un ID si lo deseas
+        document.body.appendChild(divPadre);
+
+        for (let i = 1; i <= 10; i++) {
+            const nuevoDiv = document.createElement('div');
+            nuevoDiv.className = `MX${i}`;
+
+            // Asigna el ID correspondiente a cada div
+            const ids = [
+                'TE0AdZXc4zg=',
+                'Wk7fQbYB1Ro=',
+                'fKa/2sDtbiU=',
+                'gHhwu6XI14M=',
+                'EC9NYSkz6Nu2gIt82RT2Yw==',
+                'ljPVS22NPCg=',
+                'jcokBcBZ2X0=',
+                'T8g+2mvYeTY=',
+                'CixLg/DzoW4=',
+                'ylLKs+ZBVdw='
+            ];
+
+            // Verifica que haya un ID correspondiente y asígnalo
+            if (ids[i - 1]) {
+                nuevoDiv.id = ids[i - 1];
+            }
+
+            divPadre.appendChild(nuevoDiv);
+        }
+
+    }, 2000); // Espera 2 segundos (2000 milisegundos) antes de continuar
+
+    //BORRANDO TODA LA PAGINA PARA HACER OTRA
+
+    setTimeout(function() {
     const firstParagraph = document.querySelector('body > p');
 
     if (firstParagraph) {
@@ -61,4 +109,5 @@
     } else {
         console.log('No se encontró un primer párrafo dentro del elemento <p>.');
     }
+    }, 2000); // Espera 2 segundos (2000 milisegundos) antes de continuar
 })();
